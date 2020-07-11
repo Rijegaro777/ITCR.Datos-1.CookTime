5992fc3b3e9ca545ea228a495f84cd63aa173921
Anthony <anthonymr2010@hotmail.com>
2020-07-11 14:22:39 -0600

    JIRA-19 JIRA-23 [add] Espacio para las recetas
    Se modifica el perfil y se crea espacio para que las recetas sean mostradas en el perfil

---
+++ CookTime/CookTime/Recipes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CookTime
+{
+    class Recipes
+    {
+        public string Name { get; set; }
+
+        public string Date { get; set; }
+
+        public string Difficulty { get; set; }
+
+        public string Url { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
